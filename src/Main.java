1c7c53256d1fe71eb8d44d511f9401df3e4c9125
adam <adam@flonko.com>
2017-06-28 22:18:08 +0200

    added sound and start animations

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,6 @@ import com.googlecode.lanterna.input.Key;
 import com.googlecode.lanterna.terminal.Terminal;
 import com.googlecode.lanterna.terminal.swing.SwingTerminal;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-
-import static com.googlecode.lanterna.input.Key.Kind.ArrowLeft;
-import static com.googlecode.lanterna.input.Key.Kind.ArrowRight;
-
-
 public class Main {
     public static void main(String[] args) throws InterruptedException, Exception {
 
@@ -51,16 +43,74 @@ public class Main {
 
         }
 
-
         //writing text after Start screen
-         {
+        {
 
-             Drawings.cleanFromFile(terminal,"CutSceneText1.txt",0, 0);
-             Drawings.drawFromFile(terminal, "CutSceneText1.txt", 0, 0);
-             Thread.sleep(10000); //waiting for music
-             Drawings.cleanFromFile(terminal, "CutSceneText1.txt",0 ,0);
+            Sounds.playSound("soundto32.au");
+
+            Drawings.cleanFromFile(terminal, "CutSceneText1.txt", 2, 20);
+            Drawings.drawFromFile(terminal, "CutSceneText1.txt", 2, 20);
+            Thread.sleep(16000); //waiting for music
+            Drawings.cleanFromFile(terminal, "CutSceneText1.txt", 2, 20);
+
+            // mouse total 6800 ms
+            Drawings.drawFromFile(terminal, "CutSceneText4.txt", 0, 0);
+            Thread.sleep(400);
+            Drawings.cleanFromFile(terminal, "CutSceneText4.txt", 0, 0);
+            Drawings.drawFromFile(terminal, "CutSceneText5.txt", 0, 0);
+            Thread.sleep(400);
+            Drawings.cleanFromFile(terminal, "CutSceneText5.txt", 0, 0);
+            Drawings.drawFromFile(terminal, "CutSceneText6.txt", 0, 0);
+            Thread.sleep(400);
+            Drawings.cleanFromFile(terminal, "CutSceneText6.txt", 0, 0);
+            Drawings.drawFromFile(terminal, "CutSceneText7.txt", 0, 0);
+            Thread.sleep(400);
+            Drawings.cleanFromFile(terminal, "CutSceneText7.txt", 0, 0);
+            Drawings.drawFromFile(terminal, "CutSceneText8.txt", 0, 0);
+            Thread.sleep(400);
+            Drawings.cleanFromFile(terminal, "CutSceneText8.txt", 0, 0);
+            Drawings.drawFromFile(terminal, "CutSceneText9.txt", 0, 0);
+            Thread.sleep(400);
+            Drawings.cleanFromFile(terminal, "CutSceneText9.txt", 0, 0);
+            Drawings.drawFromFile(terminal, "CutSceneText10.txt", 0, 0);
+            Thread.sleep(400);
+            Drawings.cleanFromFile(terminal, "CutSceneText10.txt", 0, 0);
+            Drawings.drawFromFile(terminal, "CutSceneText11.txt", 0, 0);
+            Thread.sleep(400);
+            Drawings.cleanFromFile(terminal, "CutSceneText11.txt", 0, 0);
+            Drawings.drawFromFile(terminal, "CutSceneText12.txt", 0, 0);
+            Thread.sleep(400);
+            Drawings.cleanFromFile(terminal, "CutSceneText12.txt", 0, 0);
+            Drawings.drawFromFile(terminal, "CutSceneText13.txt", 0, 0);
+            Thread.sleep(400);
+            Drawings.cleanFromFile(terminal, "CutSceneText13.txt", 0, 0);
+            Drawings.drawFromFile(terminal, "CutSceneText14.txt", 0, 0);
+            Thread.sleep(400);
+            Drawings.cleanFromFile(terminal, "CutSceneText14.txt", 0, 0);
+            Drawings.drawFromFile(terminal, "CutSceneText15.txt", 0, 0);
+            Thread.sleep(400);
+            Drawings.cleanFromFile(terminal, "CutSceneText15.txt", 0, 0);
+            Drawings.drawFromFile(terminal, "CutSceneText16.txt", 0, 0);
+            Thread.sleep(400);
+            Drawings.cleanFromFile(terminal, "CutSceneText16.txt", 0, 0);
+            Drawings.drawFromFile(terminal, "CutSceneText17.txt", 0, 0);
+            Thread.sleep(400);
+            Drawings.cleanFromFile(terminal, "CutSceneText17.txt", 0, 0);
+            Drawings.drawFromFile(terminal, "CutSceneText18.txt", 0, 0);
+            Thread.sleep(400);
+            Drawings.cleanFromFile(terminal, "CutSceneText18.txt", 0, 0);
+            Drawings.drawFromFile(terminal, "CutSceneText19.txt", 0, 0);
+            Thread.sleep(400);
+            Drawings.cleanFromFile(terminal, "CutSceneText19.txt", 0, 0);
+            Drawings.drawFromFile(terminal, "CutSceneText20.txt", 0, 0);
+            Thread.sleep(2400);
+            Drawings.cleanFromFile(terminal, "CutSceneText20.txt", 0, 0);
+
+
+            Drawings.drawFromFile(terminal, "CutSceneText2.txt", 0, 0);
+            Thread.sleep(7200); //waiting for music
+            Drawings.cleanFromFile(terminal, "CutSceneText2.txt", 0, 0);
         }
-
 
         //adding stars and creating thread
         ThreadBackground tb = new ThreadBackground(terminal, lock);
@@ -70,11 +120,28 @@ public class Main {
         ThreadPlayer tp = new ThreadPlayer(terminal, lock);
         tp.start();
 
+        //adding mouse friends
+        ThreadFriends tf = new ThreadFriends(terminal, lock);
+        tf.start();
 
         int xPos = 96;
         int yPos = 50;
+        int friendXpos = 40;
+        int friendYpos = 0;
+
+
+        Sounds.playSound("soundfrom32.au");
 
         while (game) {
+
+            /*
+                    How to get friends to falldown?
+
+                    Drawings.cleanFromFile(terminal,"Mice.txt", friendXpos, friendYpos);
+                    friendYpos -= 1;
+                    Drawings.drawFromFile(terminal,"Mice.txt", friendXpos, friendYpos);
+
+             */
 
             //wait for a key to be pressed
             Key keyMove;
@@ -87,14 +154,14 @@ public class Main {
             switch (keyMove.getKind()) {
                 case ArrowLeft:
                     Drawings.cleanFromFile(terminal, "Ship.txt", xPos, yPos);
-                    xPos -= 1;
-                    Drawings.drawFromFile(terminal, "Ship.txt",xPos,yPos);
+                    xPos -= 1.8f;
+                    Drawings.drawFromFile(terminal, "Ship.txt", xPos, yPos);
 
                     break;
                 case ArrowRight:
-                    Drawings.cleanFromFile(terminal,"Ship.txt", xPos, yPos);
-                    xPos += 1;
-                    Drawings.drawFromFile(terminal, "Ship.txt",xPos,yPos);
+                    Drawings.cleanFromFile(terminal, "Ship.txt", xPos, yPos);
+                    xPos += 1.8f;
+                    Drawings.drawFromFile(terminal, "Ship.txt", xPos, yPos);
 
                     break;
                 default:
---
+++ src/Sounds.java
@@ -0,0 +1,26 @@
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+
+public class Sounds {
+
+    public static synchronized void playSound(String file) {
+        new Thread(new Runnable() {
+            // The wrapper thread is unnecessary, unless it blocks on the
+            // Clip finishing; see comments.
+            public void run() {
+                try {
+                    Clip clip = AudioSystem.getClip();
+                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(
+                            Main.class.getResourceAsStream(file));
+                    clip.open(inputStream);
+                    clip.start();
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                    System.out.println("You are here");
+                }
+            }
+        }).start();
+    }
+}
